aeb4fc0a4d2f9a536eafaf33cb3e90c8d0b38f1f
Khaled Azrak <kd.azrak@gmail.com>
2025-06-24 19:54:24 +0000

    all

---
+++ app/version.py
@@ -0,0 +1,12 @@
+
+import subprocess
+
+PROJECT_NAME = "fastapi-helloworld"
+
+def get_git_hash():
+    try:
+        return subprocess.check_output(['git', 'rev-parse', 'HEAD']).decode().strip()
+    except Exception:
+        return "unknown"
+
+
